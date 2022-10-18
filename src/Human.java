b8ac3dd8e459b92ecd0ec837883cedab4b5831df
PustovayaTS <ais_ta@bk.ru>
2022-10-18 12:15:35 +0700

    Задание 1 блок 3

--- src/Human.java
+++ src/Human.java
@@ -5,10 +5,26 @@ public class Human {
     String post;
 
     public Human(int birthDate, String name, String cityOfResidence, String post) {
-        this.birthDate = birthDate;
-        this.name = name;
-        this.cityOfResidence = cityOfResidence;
-        this.post = post;
+        if (birthDate < 0) {
+            this.birthDate = 0;
+        } else {
+            this.birthDate = birthDate;
+        }
+        if (name == null) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (cityOfResidence == null) {
+            this.cityOfResidence = "Информация не указана";
+        } else {
+            this.cityOfResidence = cityOfResidence;
+        }
+        if (post == null) {
+            this.post = "Информация не указана";
+        } else {
+            this.post = post;
+        }
     }
 
     public void hello() {
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
         Human Ania = new Human(1993,"Аня", "Москва", "методист образовательных программ");
         Human Kate = new Human(1994, "Катя", "Калининград", "продакт-менеджер");
         Human Artem = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
+        Human Sasha = new Human(-3, null, null, null);
+
         Maksim.hello();
         Ania.hello();
         Kate.hello();
         Artem.hello();
+        Sasha.hello();
     }
 }
